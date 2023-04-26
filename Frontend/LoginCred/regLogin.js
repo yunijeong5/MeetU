c005739e1a2a65babeacd68df5c913d20685d35c
Nhi Ha <nha@umass.edu>
2023-04-25 23:47:33 -0400

    login CRUD/pouchdb
    - finished pouchdb
    - need to fix get endpoint issue tp://127.0.0.1:5500/signup 405 (Method Not Allowed)
    (anony

--- Frontend/LoginCred/regLogin.js
+++ Frontend/LoginCred/regLogin.js
@@ -3,23 +3,38 @@ const emailIn = document.querySelector('#email');
 const passIn = document.querySelector('#password');
 const retypeIn = document.querySelector('#retypePassword');
 
-signupForm.addEventListener('submit', (event) => {
-event.preventDefault();
-
-if (!emailIn.value || !passIn.value || !retypeIn.value) {
-    alert('Missing inputs.');
-    return;
-} 
-if (passIn.value !== retypeIn.value) {
-    alert('Passwords do not match.');
-    return;
-}
-
+signupForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    if (!emailIn.value || !passIn.value || !retypeIn.value) {
+        alert('Missing inputs.');
+        return;
+    } 
+    if (passIn.value !== retypeIn.value) {
+        alert('Passwords do not match.');
+        return;
+    }
     const user = {
         email: emailIn.value,
         password: passIn.value,
     };
-    console.log(user);
-    // redirect to another HTML page
-    window.location.href = './dashboard.html';
+    
+    // ROUTING 
+    // have user object into json 
+    try {
+        const res = await fetch('/signup', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(user),
+        });
+        // redirect to the dashboard
+        if (res.redirected) {
+            window.location.href = res.url;
+        } else {
+            alert('Failed to sign up.');
+        }
+    } catch (err) {
+        // send login error
+        console.log(err);
+        alert('Failed to sign up.');
+    }
 });
