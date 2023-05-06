93f6f715dcca2cfbefa49f8790664beaffa776ba
Nhi Ha <nha@umass.edu>
2023-05-06 10:46:52 -0400

    Fixed sign out issue
    - loads too long when attempting to redirect on directories after clicking sign out
    
    - changed to endpoint /login redirect

--- Server/index.js
+++ Server/index.js
@@ -81,11 +81,8 @@ app.post('/login', (req, res, next) => {
 
 // Handle logging out (takes us back to the login page).
 app.get('/logout', (req, res) => {
-  req.logout((err) => {
-    if (err)
-      return next(err);
-    res.redirect('/loginCred/login.html');
-  });
+  req.logout();
+  res.redirect('/login');
 });
 
 // Like login, but add a new user and password IFF one doesn't exist already.
