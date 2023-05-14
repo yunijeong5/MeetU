267d7a7ecf49fb0510b1ae2067cbd1d9721297b4
Yuni Jeong <seoyunjeong@umass.edu>
2023-05-13 22:14:00 -0400

    dashboard.js is ready. As soon as we get data from the db, dashboard will be populated with all the events associated with the user.

--- Client/js/dashboard.js
+++ Client/js/dashboard.js
@@ -1,27 +1,71 @@
 // Switch theme button functionality
 // TODO: save theme between pages
-
 const htmlDiv = document.querySelector("html");
 const themeButton = document.getElementsByClassName("theme-icon")[0];
 const navBar = document.getElementsByClassName("navbar")[0];
 const footer = document.getElementById("footer");
 
 function switchTheme() {
-  const theme = htmlDiv.getAttribute("data-bs-theme");
-  if (theme == "light") {
-    htmlDiv.setAttribute("data-bs-theme", "dark");
-    themeButton.src = "./moon-svgrepo-com.svg";
-    navBar.style.borderBottom = "1px solid #343a40";
-    footer.style.borderTop = "1px solid #343a40";
-  } else {
-    htmlDiv.setAttribute("data-bs-theme", "light");
-    themeButton.src = "./sun-svgrepo-com.svg";
-    navBar.style.borderBottom = "1px solid #e9ecef";
-    footer.style.borderTop = "1px solid #e9ecef";
-  }
+    const theme = htmlDiv.getAttribute("data-bs-theme");
+    if (theme == "light") {
+        htmlDiv.setAttribute("data-bs-theme", "dark");
+        themeButton.src = "./moon-svgrepo-com.svg";
+        navBar.style.borderBottom = "1px solid #343a40";
+        footer.style.borderTop = "1px solid #343a40";
+    } else {
+        htmlDiv.setAttribute("data-bs-theme", "light");
+        themeButton.src = "./sun-svgrepo-com.svg";
+        navBar.style.borderBottom = "1px solid #e9ecef";
+        footer.style.borderTop = "1px solid #e9ecef";
+    }
 }
 
 themeButton.addEventListener("click", switchTheme);
-const welcomeMessage = document.getElementById("lmfao");
-const username = welcomeMessage.textContent.replace("Welcome ", "").replace("!", "");
-console.log(username);
+// const welcomeMessage = document.getElementById("lmfao");
+// const username = welcomeMessage.textContent
+//     .replace("Welcome ", "")
+//     .replace("!", "");
+// console.log(username);
+
+// display all events associated with the user
+const dashboardBox = document.getElementById("dashboard");
+
+console.log(dashboardBox);
+function renderDashboard() {
+    // const events = [{}...] array of events from db
+    // TODO: I need meeting title, meeting description, meeting id, and current user id
+    const mockdata = [
+        { title: "T1", description: "text here" },
+        { title: "T2", description: "text here" },
+    ];
+
+    mockdata.forEach((eventObject) => {
+        const eventBox = document.createElement("a");
+        const titleSpan = document.createElement("span");
+        const descSpan = document.createElement("span");
+
+        eventBox.classList.add(
+            "col-12",
+            "col-sm-6",
+            "col-lg-3",
+            "col-xl-2",
+            "m-2",
+            "p-3",
+            "meeting-item"
+        );
+        eventBox.style.cursor = "pointer";
+        eventBox.style.display = "block";
+        titleSpan.textContent = eventObject["title"];
+        titleSpan.classList.add("meeting-title");
+        descSpan.textContent = eventObject["description"];
+        descSpan.classList.add("meeting-desc");
+
+        eventBox.appendChild(titleSpan);
+        eventBox.appendChild(document.createElement("br"));
+        eventBox.appendChild(descSpan);
+
+        dashboardBox.appendChild(eventBox);
+    });
+}
+
+renderDashboard();
